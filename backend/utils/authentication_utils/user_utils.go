c903527cf8f3210b113380563e29f12b32401d00
Sheryf Habib <sheryf.534@gmail.com>
2024-02-25 23:32:40 +0100

    staged the validate and hash password

--- backend/utils/authentication_utils/user_utils.go
+++ backend/utils/authentication_utils/user_utils.go
@@ -1,13 +1,32 @@
+// userUtils.go
 package userUtils
 
 import (
+	"errors"
 	"golang.org/x/crypto/bcrypt"
 )
 
-func GeneratePasswordHash(password string) (string, error) {
+func generatePasswordHash(password string) (string, error) {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
 		return "", err
 	}
 	return string(hashedPassword), nil
+}
+
+func ValidateAndHashPassword(password string) (string, bool, error) {
+	if password == "" {
+		return "", false, errors.New("password must not be empty")
+	}
+
+	if len(password) <= 5 {
+		return "", false, errors.New("password must be at least 6 characters")
+	}
+
+	hashedPassword, err := generatePasswordHash(password)
+	if err != nil {
+		return "", false, errors.New("error generating password hash")
+	}
+
+	return hashedPassword, true, nil
 }
