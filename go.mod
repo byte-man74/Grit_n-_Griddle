24304a51f494849c1bc14de903829ec5bfb0cffa
Sheryf Habib <sheryf.534@gmail.com>
2024-02-25 22:39:34 +0100

    staging go files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/byte-man74/Grit_n-_Griddle/backend
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,9 @@
+// /cmd/your-app/main.go
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Go Project!")
+}
