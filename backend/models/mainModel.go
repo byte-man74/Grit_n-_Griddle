3db529b36eb496506695984e67b9af1898aad6ee
Sheryf Habib <sheryf.534@gmail.com>
2024-02-27 21:26:26 +0100

    added food relationship

--- backend/models/mainModel.go
+++ backend/models/mainModel.go
@@ -1,16 +1,17 @@
 package models
 
 type FoodItem struct {
-	ID          int64  `json:"id"`
-	Name        string `json:"name"`
-	Is_in_Stock bool   `json:"is_in_stock"`
-	Amount      int64  `json:"amount"`
-	Description string `json:"description"`
+	ID          int64           `json:"id"`
+	Name        string          `json:"name"`
+	Is_in_Stock bool            `json:"is_in_stock"`
+	Amount      int64           `json:"amount"`
+	Description string          `json:"description"`
+	Media       []FoodItemMedia `json:"media" gorm:"foreignKey:FoodItemID"`
 }
 
 type FoodItemMedia struct {
-	//can be a link to an image or a video
-	ID       int64     `json:"id"`
-	FoodItem *FoodItem `json:"food_item"`
-	MediaUrl string    `json:"media_url"`
+	ID         int64     `json:"id"`
+	FoodItemID int64     `json:"food_item_id"`
+	FoodItem   *FoodItem `json:"food_item"`
+	MediaUrl   string    `json:"media_url"`
 }
