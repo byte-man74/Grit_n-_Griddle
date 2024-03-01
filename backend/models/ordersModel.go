a6915efce8aa6915e4cc32c82037ca6f8abd6f6c
Sheryf Habib <sheryf.534@gmail.com>
2024-03-01 15:17:52 +0100

    migration complete

--- backend/models/ordersModel.go
+++ backend/models/ordersModel.go
@@ -5,19 +5,20 @@ import (
 )
 
 type Orders struct {
-	ID            string    `json:"id"`
-	Name          string    `json:"name"`
-	DateTime      time.Time `json:"date_time"`
-	Nature        string    `json:"nature"`
-	AttendedTo    bool      `json:"attended_to"`
-	ImageURL      string    `json:"image_url"`
-	UserPhone     string    `json:"user_phone"`
-	ReferredBy    string    `json:"referred_by"`
-	PaymentStatus bool      `json:"payment_status"`
-	AmountPaid    int64     `json:"amount_paid"`
-	User          User      `gorm:"foreignKey:Phone_number" json:"user"`
-	CreatedAt     time.Time `gorm:"autoCreateTime" json:"created_at"`
-	UpdatedAt     time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+	ID            string            `json:"id"`
+	Name          string            `json:"name"`
+	DateTime      time.Time         `json:"date_time"`
+	Nature        string            `json:"nature"`
+	AttendedTo    bool              `json:"attended_to"`
+	ImageURL      string            `json:"image_url"`
+	UserPhone     string            `json:"user_phone"`
+	ReferredBy    string            `json:"referred_by"`
+	PaymentStatus bool              `json:"payment_status"`
+	AmountPaid    int64             `json:"amount_paid"`
+	User          User              `gorm:"foreignKey:Phone_number" json:"user"`
+	CreatedAt     time.Time         `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt     time.Time         `gorm:"autoUpdateTime" json:"updated_at"`
+	FoodDetail    []OrderFoodDetail `gorm:"foreignKey:ID" json:"food_detail"`
 }
 
 type OrderFoodDetail struct {
